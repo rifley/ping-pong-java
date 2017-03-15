da16c28bcd9582af262853ed66113fc542f5f3f2
Blake Womack and Riley Watts <riley@>
2017-03-15 09:54:19 -0700

    add user interface to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,14 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("I'm a ping-pong application");
+    Console myConsole = System.console();
+    System.out.println("Enter a number:");
+    String stringUserNumber = myConsole.readLine();
+    int intUserNumber = Integer.parseInt(stringUserNumber);
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+    System.out.println(pingPongResult);
   }
 }
